72301ef251112d35e7c39cbe98e48f45db099a9b
mohidqureshi <mohidq99@gmail.com>
2020-01-06 21:18:06 -0500

    Updated Layout & Information

--- external.js
+++ external.js
@@ -1,7 +1,7 @@
 var myIndex = 0;
-slide();
+carousel();
 
-function slide() {
+function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
@@ -10,5 +10,5 @@ function slide() {
     myIndex++;
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
-    setTimeout(slide, 3000); // Change image every 3 seconds
+    setTimeout(carousel, 3000); // Change image every 2 seconds
 }
