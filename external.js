2687d4551a2ccf4f8a16b1b646d71734ade560a8
mohidqureshi <mohidq99@gmail.com>
2020-09-06 17:33:40 -0400

    Updated Android app image

--- external.js
+++ external.js
@@ -1,14 +1,6 @@
-var myIndex = 0;
-carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 3000); // Change image every 2 seconds
-}
+$( document ).ready(function() {
+ // $("#div1").fadeIn(2000);
+  //$("#div2").fadeIn(2000);
+  //console.log("ready")
+});
